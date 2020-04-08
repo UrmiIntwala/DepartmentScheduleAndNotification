e696124f6a916b0315ef0db1feed8b038aac1c7d
Gohel Vishakha <gohelvishakha@gmail.com>
2020-04-08 12:32:18 +0530

    vishakha's second commit

--- myProj/views.py
+++ myProj/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render,render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate
 from myProj.models import UserDetails
 
 # Create your views here.
 class HomePageView(TemplateView):
-	def get(self,request,**kwargs):
-		return render(request,'index.html',context=None)
+	def get (self, request, **kwargs):
+		return render(request, 'index.html', context=None)
 
 def login(request):
-	return render(request,'login.html')
+	return render(request, 'index.html')
 
 def validateUser(request):
 	username=request.POST.get('username','')
+	print(username)
 	password=request.POST.get('password','')
-	user = authenticate(username=username, password=password)
+	print(password)
+	user1=UserDetails.objects.filter(user=username, pwd=password).values()
+	user = authenticate(user=username, pwd=password)
+	print(user1)
 	request.session["username"] = username
-	if user is not None:
-		request.session["role"]=user.role
+	if user1 is not None:
+		#request.session["role"]=user1.values()
+		return render(request, 'admin.html')
 	else:
-		return render(request,'Login.html')
+		return render(request, 'index.html')
